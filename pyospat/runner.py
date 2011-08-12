34275ff95bc40c5e5992e5bd874eb8e91e81bed9
Alexandre Quessy <alexandre@quessy.net>
2011-08-12 18:35:44 -0400

    add --osc-receive-port option

--- pyospat/runner.py
+++ pyospat/runner.py
@@ -20,10 +20,13 @@ def run():
     """
     parser = OptionParser(usage="%prog [options]", version="%prog " + __version__, description=DESCRIPTION)
     parser.add_option("-v", "--verbose", action="store_true", help="Makes the output verbose.")
+    parser.add_option("-p", "--osc-receive-port", type="int", help="UDP port to listen to for OSC messages.")
     (options, args) = parser.parse_args()
     config = configuration.Configuration()
     if options.verbose:
         config.verbose = True
+    if options.osc_receive_port:
+        config.osc_receive_port = options.osc_receive_port
 
     s = server.ServerWrapper(use_twisted=True)
     app = application.Application(config)
