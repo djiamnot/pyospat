06296553268f237fdf5dc085c0144cf62034e924
Alexandre Quessy <alexandre@quessy.net>
2011-08-12 13:12:17 -0400

    rename pyoscape to pyospat

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python
 """
-setup.py for Pyoscape
+setup.py for Pyospat
 Also sets up the man pages.
 
 To update the version number : 
-vim -o pyoscape/__init__.py
+vim -o pyospat/__init__.py
 """
 from setuptools import setup
 import sys
-import pyoscape
+import pyospat
 
 setup(
-    name="pyoscape",
-    version=pyoscape.__version__,
-    description="Python renderer for SpatOSC",
+    name="pyospat",
+    version=pyospat.__version__,
+    description="Python audio renderer for SpatOSC",
     author="SAT",
     author_email="alexandre@quessy.net",
-    url="http://github.com/sat-metalab/pyoscape",
-    packages=["pyoscape"],
+    url="http://github.com/sat-metalab/pyospat",
+    packages=["pyospat"],
     scripts=[]
     )
