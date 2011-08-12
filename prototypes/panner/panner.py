cb3f436aa66e046d16893675b991e4811b3ba222
Alexandre Quessy <alexandre@quessy.net>
2011-08-12 18:43:12 -0400

    update panner proto to use server.py

--- prototypes/panner/panner.py
+++ prototypes/panner/panner.py
@@ -4,21 +4,14 @@ Example of panning with pyo
 """
 import pyo
 import time
-
-SAMPLING_RATE = 44100
-NUM_CHANNELS = 2
+from pyospat import server
 
 if __name__ == "__main__":
-    s = pyo.Server(sr=SAMPLING_RATE, nchnls=NUM_CHANNELS).boot()
-    s.start()
+    s = server.ServerWrapper()
 
     noise = pyo.Noise(mul=.125)
     lfo = pyo.Sine(freq=1.0, mul=0.5, add=0.5)
     p = pyo.Pan(noise, outs=2, pan=lfo).out()
-    try:
-        while True:
-            time.sleep(0.1)
-    except KeyboardInterrupt, e:
-        print("Interrupted")
-    s.stop()
+
+    s.run()
 
