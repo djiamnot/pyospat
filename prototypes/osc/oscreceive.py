e9a24fa7b232f7c1b05f120705a00be5b912d8b3
Alexandre Quessy <alexandre@quessy.net>
2011-08-12 15:28:11 -0400

    use OscDataReceive

--- prototypes/osc/oscreceive.py
+++ prototypes/osc/oscreceive.py
@@ -4,19 +4,25 @@ Example of using a OSC receiver with Pyo.
 """
 import pyo
 import time
+from pyospat import server
 
-SAMPLING_RATE = 44100
-NUM_CHANNELS = 2
+class Example(object):
+    def __init__(self):
+        self._port = 10001
+        addresses = ["/pitch", "/amp"]
+        self._osc_receiver = pyo.OscDataReceive(self._port, addresses, self._on_message_received)
+        self._sine = pyo.Sine(freq=0.0, mul=1.0).out()
+
+    def _on_message_received(self, address, *args):
+        print("%s %s" % (address, args))
+        if address == "/pitch":
+            self._sine.setFreq(args[0])
+        elif address == "/amp":
+            self._sine.setMul(args[0])
 
 if __name__ == "__main__":
-    s = pyo.Server(sr=SAMPLING_RATE, nchnls=NUM_CHANNELS).boot()
-    s.start()
-    a = pyo.OscReceive(port=10001, address=['/pitch', '/amp'])
-    b = pyo.Sine(freq=a['/pitch'], mul=a['/amp']).out()
-    try:
-        while True:
-            time.sleep(0.1)
-    except KeyboardInterrupt, e:
-        print("Interrupted")
-    s.stop()
+    s = server.ServerWrapper()
+    example = Example()
+    s.run()
+    # why does it often end with a segmentation fault?
 
