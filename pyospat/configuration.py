9e5fef77d7949c49e87d62457a5117c534979ec6
Alexandre Quessy <alexandre@quessy.net>
2011-08-15 14:48:13 -0400

    modify header license

--- pyospat/configuration.py
+++ pyospat/configuration.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Pyospat
+# Copyright (C) 2011 Alexandre Quessy
+# Copyright (C) 2011 Michal Seta
+# All rights reserved.
+#
+# This file is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Pyospat is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Pyospat.  If not, see <http://www.gnu.org/licenses/>.
+
 """
 The Configuration class.
 """
