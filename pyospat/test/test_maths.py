11d78127941bc4ddc94ed1ea59c0a6095b93b3a1
Alexandre Quessy <alexandre@quessy.net>
2011-10-17 16:53:38 -0400

    fix an error in maths.angles_to_attenuation

--- pyospat/test/test_maths.py
+++ pyospat/test/test_maths.py
@@ -6,6 +6,8 @@ from twisted.trial import unittest
 from pyospat import maths
 import math
 
+HALF_PI = math.pi / 2.0
+
 class Test_Vector_Stuff(unittest.TestCase):
     def test_vector_length(self):
         self.failUnlessEqual(maths.length([0.0, 1.0, 0.0]), 1.0)
@@ -43,9 +45,26 @@ class Test_Audio_Spatialization(unittest.TestCase):
         def _test(speaker_aed, source_aed, expected_volume, exponent=2.0):
             self.failUnlessEqual(maths.angles_to_attenuation(speaker_aed, source_aed, exponent), expected_volume)
 
-        _test([0.0, 0.0, 1.0], [0.0, 0.0, 1.0], 1.0) # same position
-        _test([math.pi, 0.0, 1.0], [0.0, 0.0, 1.0], 0.0) # opposite
+        # same pos
+        _test([0.0, 0.0, 1.0], [0.0, 0.0, 1.0], 1.0)
 
+        # opposite
+        _test([math.pi, 0.0, 1.0], [0.0, 0.0, 1.0], 0.0)
+        _test([HALF_PI, 0.0, 1.0], [-HALF_PI, 0.0, 1.0], 0.0)
+        _test([-HALF_PI, 0.0, 1.0], [HALF_PI, 0.0, 1.0], 0.0)
+        _test([HALF_PI, HALF_PI, 1.0], [HALF_PI, -HALF_PI, 1.0], 0.0)
+
+        # one quarter
         expect = math.cos(math.pi/2.0) * 0.5 + 0.5
-        _test([math.pi / 2.0, 0.0, 1.0], [0.0, 0.0, 1.0], expect, 1.0) # one quarter
+        _test([HALF_PI, 0.0, 1.0], [0.0, 0.0, 1.0], expect, 1.0)
+        _test([-HALF_PI, 0.0, 1.0], [0.0, 0.0, 1.0], expect, 1.0)
+        _test([0.0, HALF_PI, 1.0], [0.0, 0.0, 1.0], expect, 1.0)
+        _test([0.0, 0.0, 1.0], [HALF_PI, 0.0, 1.0], expect, 1.0)
+        _test([0.0, 0.0, 1.0], [0.0, HALF_PI, 1.0], expect, 1.0)
+
+        # one eighth
+        expect = math.cos(math.pi/4.0) * 0.5 + 0.5
+        QUARTER_PI = HALF_PI / 2.0
+        _test([QUARTER_PI, 0.0, 1.0], [0.0, 0.0, 1.0], expect, 1.0)
+        _test([QUARTER_PI / 2.0, 0.0, 1.0], [-QUARTER_PI / 2.0, 0.0, 1.0], expect, 1.0)
 
