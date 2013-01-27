15137a11c6b4ea03297d006c2d53666cb390ca60
Michal Seta <mis@artengine.ca>
2013-01-27 16:50:52 -0500

    basic pluck plugin working (with pd example)

--- pyospat/plugins.py
+++ pyospat/plugins.py
@@ -586,15 +586,17 @@ class PluckedString(PyoObject):
     
     _______________________________________________________________________________________
     Author : Olivier Bélanger - 2011
+    Adapted to pyospat: Michal Seta
     _______________________________________________________________________________________
     """
-    def __init__(self, freq=220, dur=1, deviation=1, mul=0, add=1):
+    def __init__(self, freq=110, dur=1, deviation=1, mul=0.5, add=1):
         
         self._freq = freq
         self._mul = mul
         self._add = add
         self._dur = dur
         self._deviation = deviation
+        self._trig = []
         self._deviations = []
         self._tables = []
         self._impulses = []
@@ -606,21 +608,94 @@ class PluckedString(PyoObject):
         freq, dur, deviation, mul, add, lmax = convertArgsToLists(freq, dur, deviation, mul, add)
         
         for i in range (lmax):
+            self._trig.append(Trig().play())
             self._deviations.append(Randi(
                     min=0.-wrap(deviation, i), 
                     max=wrap(deviation, i), 
                     freq=random.uniform(2,4) , 
                     add=1))
             self._tables.append(CosTable([(0,0),(50,1),(300,0),(8191,0)]))
-            self._impulses.append((TrigEnv(self.trig, table=self._tables, dur=.1)))
+            self._impulses.append((TrigEnv(self._trig, table=self._tables, dur=.1)))
             self._noises.append(Biquad(Noise(self._impulses), freq=2500))
             self._outs.append(Waveguide(
-                    self.noise, 
+                    self._noises, 
                     freq=wrap(freq,i)*wrap(deviation,i), 
                     dur=wrap(dur,i), 
                     minfreq=.5, 
                     mul=wrap(mul,i)))
             self._base_objs.extend(self._outs[-1].getBaseObjects())
+
+    def __dir__(self):
+        return ["freq", "dur", "deviation", "mul", "add"]
+    
+    def setPitch(self, freq):
+        self._freq = freq
+        x, lmax = convertArgsToLists(freq)
+        [obj.setFreq(wrap(x,1)) for i, obj in enumerate(self._base_objs)]
+
+    @property
+    def freq(self):
+        """
+        Get freq property
+        @rtype : float
+        """
+        return self._freq
+
+    @property
+    def dur(self):
+        """
+        Get dur property
+        @rtype : float
+        """
+        return self._dur
+
+    @property
+    def deviation(self):
+        """
+        Get deviation property
+        @rtype : float
+        """
+        return self._deviation
+
+    @freq.setter
+    def freq(self, freq):
+        self.setPitch(freq)
+
+    @dur.setter
+    def dur(self, dur):
+        self._dur = dur
+
+    @deviation.setter
+    def deviation(self, deviation):
+        self._deviation = deviation
+
+    # TODO: add setters for mul, add, dur, deviation
+
+    # override some methods
+    def play(self, dur=0, delay=0):
+        print("running with: frequency %d" % (self._freq))
+        #self._envs.play()
+        dur, delay, lmax = convertArgsToLists(dur, delay)
+        [obj.play(wrap(dur,i),wrap(delay,i)) for i, obj in enumerate(self._trig)]
+        self._base_objs = [obj.play(wrap(dur,i), wrap(delay,i)) for i, obj in enumerate(self._base_objs)]
+        return self
+
+    def stop(self):
+        [obj.stop() for obj in self._trig]
+        [obj.stop() for obj in self._base_objs]
+
+    def out(self, chnl=0, inc=1, dur=0, delay=0):
+        dur, delay, lmax = convertArgsToLists(dur, delay)
+        [obj.play(wrap(dur,i), wrap(delay,i)) for obj in self._trig]
+        if type(chnl) == ListType:
+            self._base_objs = [obj.out(wrap(chnl,i), wrap(dur,i), wrap(delay, i)) for i, obj in enumerate(self._base_objs)]
+        else:
+            if  chnl < 0:
+                self._base_objs = [obj.out(i*inc, wrap(dur,i), wrap(delay,i)) for i, obj in enumerate(random.sample(self._base_objs, len(self._base_objs)))]
+            else:
+                self._base_objs = [obj.out(chnl+i*inc, wrap(dur,i), wrap(delay,i)) for i, obj in enumerate(self._base_objs)]
+        return self
+            
 
 class Reson(BaseSynth):
     """
