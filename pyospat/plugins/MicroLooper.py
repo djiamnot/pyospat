c218e66fc80e865153087cd194ab91e8c323e920
Michal Seta <djiamnot@gmail.com>
2013-08-04 18:14:00 -0400

    MicroLoop plugin

---
+++ pyospat/plugins/MicroLooper.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pyo import *
+
+class MicroLooper(PyoObject):
+    """
+    Loop small portions of samples from audio file. Can move within the length
+    of the file, transpose, control beginning/end loop overlap
+    """
+    def __init__(self, path=None, pitch=1., start=0., dur=1.0, mul=0.5, add=1):
+        PyoObject.__init__(self)
+
+        # instance variables
+        self._path = path
+        self._pitch = pitch
+        self._start = start
+        self._dur = dur
+        
+        path, pitch, start, dur, mul, add, lmax = convertArgsToLists(
+            self._path, 
+            self._pitch, 
+            self._start, 
+            self._dur, 
+            mul, add
+            )
+        
+        # DSP graph
+        self._table = SndTable(path=self._path)
+        self._looper = Looper(self._table, 
+                               pitch=pitch, 
+                               start=start, 
+                               dur=dur, 
+                               xfade=20, 
+                               autosmooth=True, 
+                               mul=1, 
+                               add=0)
+
+        self._base_objs = self._looper.getBaseObjects()
+
+    def __dir__(self):
+            return["path", "pitch", "start", "dur", "mul", "add"]
+
+    @property
+    def path(self):
+        return self._path
+
+    @path.setter
+    def path(self, path):
+        """
+        path: string
+        """
+        self._path = path
+        self._table.path = path
+        print("Loaded: {0}, length: {1}".format(path, str(self._table.getDur())))
+
+    @property
+    def pitch(self):
+        return self._pitch
+
+    @pitch.setter
+    def pitch(self, pitch):
+        """
+        Change reading speed
+        pitch: float, < 1 = lower, > 1 higher, 1 = original
+        """
+        self._pitch = pitch
+        self._looper.pitch = pitch
+
+    @property
+    def start(self):
+        return self._start
+
+    @start.setter
+    def start(self, start):
+        """
+        Position of the loop start within the table (seconds)
+        start: float (seconds)
+        """
+        table_duration = self._table.getDur()
+        dur = self._dur
+        
+        # if table_duration - start >= dur:
+        #     start = table_duration - dur
+        # else:
+        #     start = start % table_duration
+        print("setting MicroLooper.start to {0}".format(start))
+        self._start = start
+        self._looper.start = start
+            
+    @property
+    def dur(self):
+        return self._dur
+
+    @dur.setter
+    def dur(self, dur):
+        """
+        Loop duration
+        dur: float(seconds)
+        """
+        if dur >= self._table.getDur():
+            self._dur = self._table.getDur()
+            self._looper.dur = self._table.getDur()
+        else:
+            self._dur = dur
+            self._looper.dur = dur
+                                 
+        print("setting MicroLoope dur to {0}".format(dur))
